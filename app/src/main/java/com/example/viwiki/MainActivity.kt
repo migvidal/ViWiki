72f2a8b8a4f19305693a0dc825101f1a29181482
Miguel Vidal <68684733+migvidal@users.noreply.github.com>
2022-07-22 00:03:27 +0200

    Partial: add search button in action bar

--- app/src/main/java/com/example/viwiki/MainActivity.kt
+++ app/src/main/java/com/example/viwiki/MainActivity.kt
@@ -1,11 +1,43 @@
 package com.example.viwiki
 
+import android.app.SearchManager
+import android.content.ComponentName
+import android.content.Context
+import android.content.Intent
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.SearchView
+import com.example.viwiki.search.SearchActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        menuInflater.inflate(R.menu.action_bar_menu, menu)
+
+        val searchItem: MenuItem = menu.findItem(R.id.action_search)
+        val searchView = searchItem.actionView as SearchView
+
+        val searchManager: SearchManager = getSystemService(Context.SEARCH_SERVICE) as SearchManager
+        val componentName = ComponentName(this, SearchActivity::class.java)
+
+        searchView.setSearchableInfo(searchManager.getSearchableInfo(componentName))
+
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.action_search -> {
+                startActivity(Intent(SEARCH_SERVICE))
+                return true
+            }
+            else -> return super.onOptionsItemSelected(item)
+        }
     }
 }
