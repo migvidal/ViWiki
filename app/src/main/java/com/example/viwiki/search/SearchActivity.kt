1016962e0cc2326999a2d957ec5abc0faf1f8661
Miguel Vidal <68684733+migvidal@users.noreply.github.com>
2022-07-21 19:23:03 +0200

    Create adapter for search results

--- app/src/main/java/com/example/viwiki/search/SearchActivity.kt
+++ app/src/main/java/com/example/viwiki/search/SearchActivity.kt
@@ -3,32 +3,40 @@ package com.example.viwiki.search
 import android.app.SearchManager
 import android.content.Intent
 import android.os.Bundle
-import android.widget.TextView
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.Observer
 import com.example.viwiki.R
+import com.example.viwiki.databinding.ActivitySearchBinding
 
 class SearchActivity : AppCompatActivity() {
 
     val viewModel: SearchViewModel by viewModels()
+    val binding: ActivitySearchBinding by lazy {
+        ActivitySearchBinding.inflate(layoutInflater)
+    }
+    lateinit var adapter: SearchAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_search)
-
-        var testTextView = findViewById<TextView>(R.id.tv_test)
+        setContentView(binding.root)
 
         // Verify intent
         if (intent.action == Intent.ACTION_SEARCH) {
             intent.getStringExtra(SearchManager.QUERY)?.also { query ->
-                viewModel.searchResponse.observe(this, Observer {
-                    testTextView.text = it.query?.search?.get(0)?.title.toString()
+                viewModel.searchResponse.observe(this, Observer { response ->
+                    if (response.query !== null) {
+                        setupRecyclerView(response.query.search)
+                    }
                 })
                 viewModel.searchArticles(query)
-                // TODO recyclerview + viewbinding
             }
         }
+    }
+
+    private fun setupRecyclerView(resultList: List<SearchResponse.SearchQuery.Search>) {
+        adapter = SearchAdapter(this, resultList)
+        binding.resultsList.adapter = adapter
     }
 
 }
---
+++ app/src/main/java/com/example/viwiki/search/SearchAdapter.kt
@@ -0,0 +1,49 @@
+package com.example.viwiki.search
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.databinding.DataBindingUtil
+import androidx.recyclerview.widget.RecyclerView
+import androidx.recyclerview.widget.RecyclerView.Adapter
+import com.example.viwiki.R
+import com.example.viwiki.databinding.ResultListItemBinding
+
+class SearchAdapter(val context: SearchActivity,
+                    var dataSet: List<SearchResponse.SearchQuery.Search>)
+    : Adapter<SearchAdapter.SearchViewHolder>() {
+
+    /**
+     * Used by onCreateViewHolder
+     */
+    inner class SearchViewHolder(val binding: ResultListItemBinding) : RecyclerView.ViewHolder(binding.root) {
+        var searchResult = binding.searchResult
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): SearchViewHolder {
+        // TODO with data binding
+        val binding = DataBindingUtil.inflate<ResultListItemBinding>(
+            LayoutInflater.from(context), R.layout.result_list_item, parent, false
+        )
+        return SearchViewHolder(binding)
+    }
+
+    override fun onBindViewHolder(holder: SearchViewHolder, position: Int) {
+        holder.apply {
+            searchResult = dataSet[position]
+            holder.binding.executePendingBindings()
+        }
+        // onclick listener...
+    }
+
+    override fun getItemCount(): Int {
+        return dataSet.size
+    }
+
+    fun updateResults(dataSet: List<SearchResponse.SearchQuery.Search>) {
+        this.dataSet = dataSet
+        notifyDataSetChanged()
+    }
+}
